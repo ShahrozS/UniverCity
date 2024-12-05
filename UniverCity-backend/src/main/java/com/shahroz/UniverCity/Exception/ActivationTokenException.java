c571747fdb0cefbd381801ec5ea1d7529a5bf885
ShahrozS <shahrozsalman7@gmail.com>
2024-12-05 23:29:29 +0500

    Implement Predefined Global Exception Handling.

---
+++ UniverCity-backend/src/main/java/com/shahroz/UniverCity/Exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.shahroz.UniverCity.Exception;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
