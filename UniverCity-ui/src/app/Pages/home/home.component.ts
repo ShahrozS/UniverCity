74b857ca5ff72268b26d28f7acaafedb4bb4dcca
PapeyLesgoo <116169561+PapeyLesgoo@users.noreply.github.com>
2024-12-10 16:20:12 +0500

    filter connection to home and university list component

--- UniverCity-ui/src/app/Pages/home/home.component.ts
+++ UniverCity-ui/src/app/Pages/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { UniversityByProgramListComponent } from '../university-by-program-list/university-by-program-list.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  
 })
 export class HomeComponent {
   rangeValues: number[] = [0, 735]
+  selectedFilters: any = {}; // Store filters to pass to the university list
 
+  onFiltersChanged(filters: any): void {
+    console.log('Filters updated:', filters);
+    this.selectedFilters = filters;
+  }
  
 }
