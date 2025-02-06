92d188d1d2dccde57e350bc70245175218704d1e
ShahrozS <shahrozsalman7@gmail.com>
2025-02-07 01:00:02 +0500

    Create categories frontend

---
+++ UniverCity-ui/src/app/Pages/quiz/quiz-category/quiz-category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-category',
+  templateUrl: './quiz-category.component.html',
+  styleUrl: './quiz-category.component.scss'
+})
+export class QuizCategoryComponent {
+
+}
