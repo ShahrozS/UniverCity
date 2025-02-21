b94f9981d416693018d26b190f6008d3cb17a761
ShahrozS <shahrozsalman7@gmail.com>
2025-02-21 11:39:41 +0500

    before switching to notifications

--- UniverCity-backend/src/main/java/com/shahroz/UniverCity/Repositories/UserFavouriteRepository.java
+++ UniverCity-backend/src/main/java/com/shahroz/UniverCity/Repositories/UserFavouriteRepository.java
@@ -1,6 +1,5 @@
 package com.shahroz.UniverCity.Repositories;
 
-import com.shahroz.UniverCity.Entities.UserFavourite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserFavouriteRepository  extends JpaRepository<UserFavourite, Long> {
