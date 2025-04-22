51287148864bf8122f6717eb386ee0a3dc9bb4f9
ShahrozS <shahrozsalman7@gmail.com>
2025-04-22 23:37:23 +0500

    added fav university functionality on the university details page aswell.

--- UniverCity-ui/src/app/Pages/university-details/university-details.component.ts
+++ UniverCity-ui/src/app/Pages/university-details/university-details.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
+import { FavouritesService } from '../../Services/services';
 
 @Component({
   selector: 'app-university-details',
@@ -8,8 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UniversityDetailsComponent implements OnInit {
   university: any;
-  
-  constructor(private route: ActivatedRoute){
+  faHeart = faHeart;
+  isFavourite = false;
+  isFavorite = false;
+
+
+
+  constructor(private route: ActivatedRoute,
+            private favouriteUniversityService: FavouritesService,
+            private cdr: ChangeDetectorRef
+            
+        
+  ){
   
   }
   
@@ -19,4 +31,33 @@ export class UniversityDetailsComponent implements OnInit {
       console.log(this.university);
     })
   }
+
+  toggleFavorite() {
+    if (!this.isFavorite) {
+      this.favouriteUniversityService.addFavorite({ universityId: this.university.id });
+      this.isFavorite = true;
+    }
+  }
+
+  favUni(event: Event) {
+    event.stopPropagation();
+
+    const universityId = this.university.id;
+    console.log(this.university);
+    console.log(universityId);
+    if (!this.isFavorite) {
+      this.favouriteUniversityService.addFavorite({ universityId: this.university.id }).subscribe(
+        () => {
+          console.log("Added to favorites!");
+          this.isFavorite = true;
+          this.cdr.detectChanges(); // 🔥 Ensure UI updates immediately
+        },
+        (error) => {
+          console.error("Error favoriting:", error);
+        }
+      );
+    }
+  }
+
+
 }
